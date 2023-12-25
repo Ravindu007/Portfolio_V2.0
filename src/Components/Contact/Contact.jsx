4d0607bba464ffaf854d7d94fbadd552b46620e1
Ravindu007 <ravindu0504@gmail.com>
2023-12-25 20:51:10 +0530

    email sending option

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,12 +1,81 @@
-import React from 'react'
+import React, { useState , useRef} from 'react'
+import {motion} from 'framer-motion'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+
+  const [social, setSocial] = useState([
+    {id:1, link:"", logo:"./icons/linkedIn.png"},
+    {id:2, link:"", logo:"./icons/github.png"},
+    {id:3, link:"", logo:"./icons/fb.png"},
+    {id:4, link:"", logo:"./icons/twitter.png"}
+  ])
+
+  const form = useRef();
+
+  const [name, setName] = useState("")
+  const [email,setEmail] = useState("")
+  const[message,setMessage] = useState("")
+
+  const [showstate, setShowState] = useState(false)
+  const [error, setError] = useState(false)
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setName("")
+    setEmail("")
+    setMessage("")
+    emailjs.sendForm('service_b1gwgfo', 'template_gxzjm6l', form.current, 'dIoRSsrphw9avWH4n')
+      .then(() => {
+        setError(false)
+        setShowState(true)
+      }, (error) => {
+        setError(true)
+        setShowState(true)
+      });
+    };
+
+  const variants = {
+    inputInitial:{
+      opacity:0,
+      x:100,
+    },
+    inputAnimate:{
+      opacity:1,
+      x:0,
+      transition:{
+        type:"spring",
+        stiffness:400,
+        damping:10,
+        staggerChildren:0.4
+      },
+    },
+    leftInitial:{
+      opacity:0,
+      y:100,
+    },
+    leftAnimate:{
+      opacity:1,
+      y:0,
+      transition:{
+        type:"spring",
+        stiffness:400,
+        damping:10,
+        staggerChildren:0.4
+      }
+    },
+    hoverIcon:{
+      scale:0.75
+    },
+  }
+
+
   return (
     <div className='contact px-10 py-6'>
       <div className="grid grid-cols-2">
         {/* contact me */}
-        <div className="col-span-2 md:col-span-1 flex flex-col gap-y-10">
-          <h1 className='text-7xl'>CONTACT ME</h1>
+        <div className="col-span-2 md:col-span-1 flex flex-col gap-y-5">
+          <h1 className='text-4xl font-semibold md:font-bold md:text-7xl'>CONTACT ME</h1>
 
           <div className="address">
             <p>Ravindu Dharmadasa</p>
@@ -16,38 +85,43 @@ const Contact = () => {
             <p>Sri Lanka</p>
           </div>
 
-          <div className="social-media flex gap-5">
-            <a href=""><img src="./icons/linkedIn.png" alt="" width={25}/></a>
-            <a href=""><img src="./icons/github.png" alt="" width={25}/></a>
-            <a href=""><img src="./icons/fb.png" alt="" width={25}/></a>
-            <a href=""><img src="./icons/twitter.png" alt="" width={25}/></a>
+          <div className="social-media flex gap-5 mb-20">
+            {social.map((item) => (
+              <motion.a key={item.id} href={item.link}  variants={variants} initial="leftInitial"   whileInView="leftAnimate">
+                <motion.img variants={variants} whileHover="hoverIcon" src={item.logo} alt="" width={25}/>
+              </motion.a>
+            ))}
           </div>
         </div>
 
         {/* email */}
-        <div className="col-span-2 md:col-span-1">
-          <form action="">
-              <div className="form-group flex flex-col">
+        <motion.div className="col-span-2 md:col-span-1" variants={variants}>
+          <form ref={form} onSubmit={sendEmail}>
+
+              <motion.div className="form-group flex flex-col" variants={variants} initial="inputInitial" whileInView="inputAnimate">
                 <label>Name</label>
-                <input type="text" className='rounded-md py-1 px-1 bg-gray-50 text-black' placeholder='Enter Your Name Here' />
-              </div>
+                <input type="text" className='rounded-md py-1 px-1 bg-gray-50 text-black' placeholder='Enter Your Name Here' name='user_name' value={name} onChange={(e)=>{setName(e.target.value)}}/>
+              </motion.div>
 
-              <div className="form-group flex flex-col">
+              <motion.div className="form-group flex flex-col" variants={variants} initial="inputInitial" whileInView="inputAnimate">
                 <label>Email</label>
-                <input type="text" className='rounded-md py-1 px-1 bg-gray-50 text-black' placeholder='Enter Your Email address Here' />
-              </div>
+                <input type="text" className='rounded-md py-1 px-1 bg-gray-50 text-black' placeholder='Enter Your Email address Here' name='user_email' value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+              </motion.div>
 
-              <div className="form-group flex flex-col">
+              <motion.div className="form-group flex flex-col" variants={variants} initial="inputInitial" whileInView="inputAnimate">
                 <label>Message</label>
-                <textarea name="" id="" cols="30" rows="5" className='rounded-md py-1 px-1 bg-gray-50 text-black' placeholder='Leave a message for me'></textarea>
-              </div>
+                <textarea id="" cols="30" rows="5" className='rounded-md py-1 px-1 bg-gray-50 text-black' placeholder='Leave a message for me' name='user_message' value={message} onChange={(e)=>{setMessage(e.target.value)}}></textarea>
+              </motion.div>
               <div className="flex justify-end items-center gap-2">
                 <p>SEND TO: <span className='text-primary'>ravindu0504@gmail.com</span></p>
-                <button className='styleBtn'>SEND</button>
+                <button className='styleBtn' type="submit" value="Send">SEND</button>
+              </div>
+              <div className="message flex justify-center font-semibold">
+                <p>{showstate ? (error ? "Sorry could not send the Message": "Message sent successfully"): ""}</p>
               </div>
           </form>
 
-        </div>
+        </motion.div>
       </div>
     </div>
   )
