13214079f2bb54d0c428d3bb5b1db030dd13a958
Ravindu007 <ravindu0504@gmail.com>
2024-01-01 22:14:01 +0530

    new details added

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,7 +6,15 @@ import { motion } from 'framer-motion';
 const Projects = () => {
   const [projects, setProjects] = useState([
 
-    { id: 1, name: 'Easy Proof', desc: 'A web application that ensures the authenticity of a product by saving product details in a blockchain and allowing QR scan functionality to check authenticity', img: './projects/easy.png', techsUsed:[
+    { id: 1, name: 'TaskCraft', desc: 'A collobarative task management application which allows team members to work on different tasks colloboratively', img: './projects/taskcraft.png', techsUsed:[
+      {id:1, techName:"ASP.NET", logo:"./programming/csharp.svg"},
+      {id:2, techName:"Angular", logo:"./programming/angular.svg"},
+      {id:3, techName:"MySQL", logo:"./programming/mysql.svg"},
+      {id:3, techName:"Auth0", logo:"./programming/auth0.svg"},
+      {id:4, techName:"Git", logo:"./programming/git.svg"},
+    ], github: 'https://github.com/Ravindu007/TaskCraft' },
+
+    { id: 2, name: 'Easy Proof', desc: 'A web application that ensures the authenticity of a product by saving product details in a blockchain and allowing QR scan functionality to check authenticity', img: './projects/easy.png', techsUsed:[
       {id:1, techName:"ASP.NET", logo:"./programming/csharp.svg"},
       {id:2, techName:"Angular", logo:"./programming/angular.svg"},
       {id:3, techName:"MySQL", logo:"./programming/mysql.svg"},
@@ -14,7 +22,7 @@ const Projects = () => {
     ], github: 'https://github.com/Ravindu007/EasyProof-Frontend' },
 
 
-    { id: 2, name: 'Employee Management System', desc: 'A platform that allows a human resource department to manage their employees in the company', img: './projects/employee.png', techsUsed:[
+    { id: 3, name: 'Employee Management System', desc: 'A platform that allows a human resource department to manage their employees in the company', img: './projects/employee.png', techsUsed:[
       {id:1, techName:"ASP.NET", logo:"./programming/csharp.svg"},
       {id:2, techName:"Angular", logo:"./programming/angular.svg"},
       {id:3, techName:"MySQL", logo:"./programming/mysql.svg"},
@@ -22,7 +30,7 @@ const Projects = () => {
     ], github: 'https://github.com/Ravindu007/Employee-Management-CRUD' },
 
 
-    { id: 3, name: 'Learning Management System', desc: 'A platform that allows managing academic tasks for students and teachers controlled with a secure admin panel.', img: './projects/lms.png', techsUsed:[
+    { id: 4, name: 'Learning Management System', desc: 'A platform that allows managing academic tasks for students and teachers controlled with a secure admin panel.', img: './projects/lms.png', techsUsed:[
       {id:1, techName:"React", logo:"./programming/react.svg"},
       {id:2, techName:"Node.js", logo:"./programming/node.svg"},
       {id:3, techName:"Express.js", logo:"./programming/express.svg"},
@@ -30,7 +38,7 @@ const Projects = () => {
       {id:5, techName:"Git", logo:"./programming/git.svg"},
     ], github: 'https://github.com/Ravindu007/MERN-LMS' },
 
-    { id: 4, name: 'Reservation Web Application', desc: 'A hotel reservation web application that allows users to manage reservations', img: './projects/hotel.png', techsUsed:[
+    { id: 5, name: 'Reservation Web Application', desc: 'A hotel reservation web application that allows users to manage reservations', img: './projects/hotel.png', techsUsed:[
       {id:1, techName:"React", logo:"./programming/react.svg"},
       {id:2, techName:"Node.js", logo:"./programming/node.svg"},
       {id:3, techName:"Express.js", logo:"./programming/express.svg"},
@@ -39,7 +47,7 @@ const Projects = () => {
     ], github: 'https://github.com/Ravindu007/MERN-Booking.com-clone' },
 
 
-    { id: 5, name: 'GPA Calculator', desc: 'A web application that allow undergraduates to add their academic marks and view their current GPA', img: './projects/gpa.png', techsUsed:[
+    { id: 6, name: 'GPA Calculator', desc: 'A web application that allow undergraduates to add their academic marks and view their current GPA', img: './projects/gpa.png', techsUsed:[
       {id:1, techName:"React", logo:"./programming/react.svg"},
       {id:2, techName:"Node.js", logo:"./programming/node.svg"},
       {id:3, techName:"Express.js", logo:"./programming/express.svg"},
@@ -48,7 +56,7 @@ const Projects = () => {
     ], github: 'https://github.com/Ravindu007/GPA---Calculator' },
 
     
-    { id: 6, name: 'TraveLog', desc: 'A web plat form which connects travellers around the world', img: './projects/travel.png', techsUsed:[
+    { id: 7, name: 'TraveLog', desc: 'A web plat form which connects travellers around the world', img: './projects/travel.png', techsUsed:[
       {id:1, techName:"React", logo:"./programming/react.svg"},
       {id:2, techName:"Node.js", logo:"./programming/node.svg"},
       {id:3, techName:"Express.js", logo:"./programming/express.svg"},
@@ -56,7 +64,7 @@ const Projects = () => {
       {id:5, techName:"Git", logo:"./programming/git.svg"},
     ], github: 'https://github.com/Ravindu007/travelog-frontend' },
 
-    { id: 7, name: 'Weather Application', desc: 'A Personalized weather application which shows weather according to user input', img: './projects/weather.png', techsUsed:[
+    { id: 8, name: 'Weather Application', desc: 'A Personalized weather application which shows weather according to user input', img: './projects/weather.png', techsUsed:[
       {id:1, techName:"React", logo:"./programming/react.svg"},
       {id:2, techName:"Node.js", logo:"./programming/node.svg"},
       {id:3, techName:"Express.js", logo:"./programming/express.svg"},
